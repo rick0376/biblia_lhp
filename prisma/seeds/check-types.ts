a1def58d657505ba979bca6adecb4322a7de88fc
rick0376 <seu-email@dominio.com>
2026-01-26 09:47:58 -0300

    first commit

--- prisma/seeds/check-types.ts
+++
@@ -1,8 +0,0 @@
-// prisma/check-types.ts
-import { Prisma } from "@prisma/client";
-
-type X = Prisma.HymnUpdateInput;
-// se chorus existir, você consegue referenciar sem erro:
-const ok: X = { chorus: "teste" };
-
-console.log("OK");
